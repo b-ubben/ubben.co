dfae58cca0b97082dce9e9c6f7a54917c142a7c2
Brandon Ubben <ubben@pop-os.localdomain>
2019-09-20 11:54:13 -0700

    Font fix, started on nav

--- public/bundle.js
+++ public/bundle.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39611" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46117" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
