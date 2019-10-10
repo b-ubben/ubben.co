36b69d0e254aa6b6d3e147ba4074e2c515464ce3
b-ubben <brackben@gmail.com>
2019-10-09 22:05:51 -0700

    Adding pages slowly, tweaking nav styles for betterment of humanity

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,4 +1,4 @@
-const menuButton = document.querySelector('.nav__menu-button');
+const menuButton  = document.querySelector('.nav__menu-button');
 const closeButton = document.querySelector('.nav__close-button');
 const navLinksList = document.querySelector('.nav__links-list');
 const container = document.querySelector('.container');
@@ -6,12 +6,10 @@ const container = document.querySelector('.container');
 
 function hideNav() {
   navLinksList.classList.remove('show', 'nav__links-list--expanded');
-  navLinksList.classList.add('hidden');
 }
 
 function showNav() {
   navLinksList.classList.add('show', 'nav__links-list--expanded');
-  navLinksList.classList.remove('hidden');
 }
 
 menuButton.addEventListener('click', e => {
